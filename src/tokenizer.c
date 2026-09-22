e26432d6107d00916b0a0378503a9e48c95efb96
JustinCodesByHand <jcmacey2003@gmail.com>
2026-09-22 00:42:29 -0400

    test2

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// aaaaccept Lexer struct some changes
+// aaaaccept Lexer struct some changesaaa
 // returns a scanner call and passes &lex
 static char peek(const Lexer *foo) {
 
